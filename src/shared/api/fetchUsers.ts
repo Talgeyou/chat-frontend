cbe0e4f9a501646420d3a5f8edde6a6d2f7b43f4
talge <talgeyou@gmail.com>
2023-04-09 11:35:36 +1000

    fix: typo in request

--- src/shared/api/fetchUsers.ts
+++ src/shared/api/fetchUsers.ts
@@ -1,5 +1,5 @@
 import { BASE_API_URL } from './constants';
 
 export async function fetchUsers() {
-  return await fetch(`${BASE_API_URL}/users`).then((res) => res.json());
+  return await fetch(`${BASE_API_URL}users`).then((res) => res.json());
 }

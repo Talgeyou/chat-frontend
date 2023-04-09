a41598c4527de3656892f7e84754a4b280d59ae1
talge <talgeyou@gmail.com>
2023-04-09 14:43:49 +1000

    fix: auto scroll to bottom

--- src/widgets/chat/ui/ChatContent.tsx
+++ src/widgets/chat/ui/ChatContent.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import { MessageWithUser } from '@/widgets/chat/types';
 import clsx from 'clsx';
 import { useSession } from 'next-auth/react';
@@ -10,11 +10,23 @@ type Props = {
 function ChatContent({ messages }: Props) {
   const { data } = useSession();
 
+  const [lastMessageElement, setLastMessageElement] =
+    useState<HTMLLIElement | null>(null);
+
+  useEffect(() => {
+    if (lastMessageElement) {
+      lastMessageElement.scrollIntoView();
+    }
+  }, [lastMessageElement]);
+
   return (
-    <div className="p-4 flex-1 w-full overflow-y-auto overflow-x-hidden scrollbar scrollbar-thin scrollbar-thumb-purple-500">
+    <div className="p-4 flex-1 w-full overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-purple-500">
       <ul className="flex flex-col gap-8 overflow-x-hidden">
-        {messages.map((message) => (
+        {messages.map((message, i) => (
           <li
+            ref={(element) =>
+              i === messages.length - 1 && setLastMessageElement(element)
+            }
             key={message.id}
             className={clsx('flex flex-col gap-2', {
               'items-end': data?.user.id === message.userId,
